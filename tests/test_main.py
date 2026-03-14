69ec577b17705bd62d8257e67519fba518a2b606
Will <rwilliamdaly@gmail.com>
2026-03-14 08:51:27 -0400

    Add tests/test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+"""
+Tests for 2.9 Case Study: Representing and Manipulating Data with Vectors and Matrices in Python
+"""
+import pytest
+from main import main
+
+
+def test_main():
+    """Test the main function."""
+    # TODO: Add your test cases here
+    pass
